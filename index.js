e88cff85e4a169fd0c63ee7ce06b17cebae41036
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-02-08 11:45:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let weatherDisplay = document.getElementById("weather-display")
 
 getWeatherBtn.addEventListener("click", function (){
     
+    if (cityInput.value !== "") {
     
     fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=e23500ef96a823d95fb0ddc49db5b795&units=imperial`) 
         .then (response => response.json())
@@ -15,7 +16,7 @@ getWeatherBtn.addEventListener("click", function (){
             
         cityDisplay.innerHTML = data.name
         weatherDisplay.innerHTML = `${Math.floor (data.main.temp)}°` 
-    })
+    })}
     
     cityInput.value = ""
     
