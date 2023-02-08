dcce87b4763944a39c87fc2f1e742bb8aaa05eba
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-02-08 11:05:17 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const cityInput = document.getElementById("city-input")
+const getWeatherBtn = document.getElementById("get-weather-btn")
+let cityDisplay = document.getElementById("city-name-display")
+let weatherDisplay = document.getElementById("weather-display")
+    
+    
+    
+
+getWeatherBtn.addEventListener("click", function (){
+    
+    
+    fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=e23500ef96a823d95fb0ddc49db5b795&units=imperial`) 
+        .then (response => response.json())
+        .then (data => {
+            
+        cityDisplay.innerHTML = data.name
+        weatherDisplay.innerHTML = `${Math.floor (data.main.temp)}°` 
+    })
+    
+    cityInput.value = ""
+    
+})
+
+        
+        
+
+
+
+
+
+
+
+//e23500ef96a823d95fb0ddc49db5b795
